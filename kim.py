a5861dc2968f987636c02eb997a5fe16b82841a7
Altruist13 <39981689+Altruist13@users.noreply.github.com>
2019-06-21 18:49:22 +0530

    Add files via upload

---
+++ kim.py
@@ -0,0 +1,39 @@
+import time
+import datetime
+import os
+import subprocess
+
+import azure
+from azure.storage.blob import BlockBlobService
+from azure.storage.blob import ContentSettings
+
+block_blob_service = BlockBlobService(account_name='hydroponicssa' , account_key='aaIPKVI21tc04BRE7DOX9XTTR3CvS8B2l987S8mJ7jdlJ7vNkJuhEqQakDWIpXcj6u9fKihB6xUxlLGspV8Zew==')
+
+class camera:
+    def __init__(self,n=None,m=None):
+        self.n = n
+        self.m = m
+    
+    def default(self):
+        while True:
+            f=datetime.datetime.now()
+            os.system(('fswebcam -F 20 -S 3 --jpeg 50 --no-banner --save /home/pi/Downloads/redirect/{}').format(f.strftime("%H%M%S")))
+            block_blob_service.create_blob_from_path('hydroponics','{}'.format(f.strftime("%H%M%S")),'/home/pi/Downloads/redirect/{}'.format(f.strftime("%H%M%S")),content_settings=ContentSettings(content_type = 'image/jpeg'))
+            time.sleep(60)
+            os.system(('fswebcam  -F 20 -S 3 --jpeg 50 --no-banner --rotate 90 --save /home/pi/to_transmit/%H%M%S.jpg'))
+            block_blob_service.create_blob_from_path('hydroponics','{}'.format(f.strftime("%H%M%S")),'/home/pi/Downloads/redirect/{}'.format(f.strftime("%H%M%S")),content_settings=ContentSettings(content_type = 'image/jpeg'))
+            time.sleep(60)
+            os.system('fswebcam  -F 20 -S 3 --jpeg 50 --no-banner --invert --save /home/pi/to_transmit/%H%M%S.jpg')
+            block_blob_service.create_blob_from_path('hydroponics','{}'.format(f.strftime("%H%M%S")),'/home/pi/Downloads/redirect/{}'.format(f.strftime("%H%M%S")),content_settings=ContentSettings(content_type = 'image/jpeg'))
+            time.sleep(60)
+            os.system('fswebcam  -F 20 -S 3 --jpeg 50 --no-banner --greyscale --save /home/pi/to_transmit/%H%M%S.jpg')
+            block_blob_service.create_blob_from_path('hydroponics','{}'.format(f.strftime("%H%M%S")),'/home/pi/Downloads/redirect/{}'.format(f.strftime("%H%M%S")),content_settings=ContentSettings(content_type = 'image/jpeg'))
+            time.sleep(60)
+            os.system(('fswebcam  -F 20 -r 1900*1080 --no-banner -S 3 --jpeg 50  --save /home/pi/to_transmit/%H%M%S.jpg'))
+            block_blob_service.create_blob_from_path('hydroponics','{}'.format(f.strftime("%H%M%S")),'/home/pi/Downloads/redirect/{}'.format(f.strftime("%H%M%S")),content_settings=ContentSettings(content_type = 'image/jpeg'))
+            time.sleep(60)
+
+cam=camera()
+cam.default()
+        
+
